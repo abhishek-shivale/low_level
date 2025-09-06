64d64508f4ac8b1e1c4d688972f39bfb5e1e7a52
Abhishek shivale <abhishekshivale45@gmail.com>
2025-09-06 19:01:17 +0530

    continuing on student report

--- project/studentReportCard.cpp
+++ project/studentReportCard.cpp
@@ -15,24 +15,75 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+struct subject {
+    int maths;
+    int english;
+    int science;
+    int history;
+    int geography;
+};
+
+struct student {
+    string roll_no;
+    string name;
+    string grade;
+    string section;
+    subject subject;
+};
+
 int main() {
+    vector<student> students;
 
-    string student_name, student_roll_no, student_grade, student_section, student_subject;
+    char choice;
 
-    cout << "Enter the name of the student: ";
-    cin >> student_name;
-    cout << "Enter the roll number of the student: ";
-    cin >> student_roll_no;
-    cout << "Enter the grade of the student: ";
-    cin >> student_grade;
-    cout << "Enter the section of the student: ";
-    cin >> student_section;
-    cout << "Enter the subject of the student: ";
-    cin >> student_subject;
+    do {
+        student s;
 
-    cout << student_name << endl;
 
+        cout << "Enter the name of the student: ";
+        cin >> s.name;
+        cout << "Enter the roll number of the student: ";
+        cin >> s.roll_no;
+        cout << "Enter the grade of the student: ";
+        cin >> s.grade;
+        cout << "Enter the section of the student: ";
+        cin >> s.section;
+
+        cout << "Enter marks in Maths: ";
+        cin >> s.subject.maths;
+        cout << "Enter marks in English: ";
+        cin >> s.subject.english;
+        cout << "Enter marks in Science: ";
+        cin >> s.subject.science;
+        cout << "Enter marks in History: ";
+        cin >> s.subject.history;
+        cout << "Enter marks in Geography: ";
+        cin >> s.subject.geography;
+
+        students.push_back(s);
+
+
+        cout << "Student Record added successfully!\n";
+        cout << "Do you want to add another student? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "\n--- Student Report Cards ---\n";
+    for (auto &s: students) {
+        cout << "Name: " << s.name << "\n";
+        cout << "Roll No: " << s.roll_no << "\n";
+        cout << "Grade: " << s.grade << "  Section: " << s.section << "\n";
+        cout << "Maths: " << s.subject.maths << "\n";
+        cout << "English: " << s.subject.english << "\n";
+        cout << "Science: " << s.subject.science << "\n";
+        cout << "History: " << s.subject.history << "\n";
+        cout << "Geography: " << s.subject.geography << "\n";
+        cout << "--------------------------\n";
+    }
+
+    return 0;
 }
