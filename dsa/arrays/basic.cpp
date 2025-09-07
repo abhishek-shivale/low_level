3fb061231d86b26fd0c15edec04dc48eb80bc88a
Abhishek shivale <abhishekshivale45@gmail.com>
2025-09-07 15:54:58 +0530

    started on dsa array

---
+++ dsa/arrays/basic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int newArray[3][5] = {
+        {1, 2, 3, 4, 5},
+        {2, 4, 6, 8, 10},
+        {1, 3, 5, 7, 9}
+    };
+}
